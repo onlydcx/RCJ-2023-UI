0e9799b7a3ec9bcc8b70b8625a71c88ea977e823
= <95466399+rcj-hana@users.noreply.github.com>
2022-12-20 22:39:13 +0900

    I2C シンプル

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,65 @@
 #include "Arduino.h"
 #include <Wire.h>
+#include <stdlib.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
 
-#define SLAVE_ADDRESS 0x01
-#define SLAVE_REGISTER_1 0x00
-#define SLAVE_REGISTER_2 0x01
 
-uint8_t registerIndex = 0;
-uint8_t data[2] = {0, 1};
+Adafruit_ST7789 tft = Adafruit_ST7789(6, 8, 4);
 
-void receiveEvent() {
-  while(Wire.available() > 0){
-    registerIndex = Wire.read();
-  }
+float p = 3.1415926;
+
+void clear() {
+   tft.fillScreen(ST77XX_BLACK);
 }
 
-void requestEvent() {
-  Wire.write(data[registerIndex]);
+#define w tft.width()
+#define h tft.height()
+
+#define char_w 5
+#define char_h 7
+
+#define lb A10
+#define cb A0
+#define rb A1
+
+byte data = 0;
+
+void DataRequest() {
+  Wire.write(digitalRead(lb));
 }
 
 void setup() {
-Wire.begin(0x01);
-Wire.onReceive(receiveEvent);
-Wire.onRequest(requestEvent);
+   Wire.begin(8);
+   Wire.onRequest(DataRequest);
+   Serial.begin(9600);
+   tft.init(135, 240);
+   clear();
+   tft.setRotation(3);
+   tft.drawRect(0,h/2-30,60,60,ST7735_WHITE);
+   tft.drawRect(w/2-30,h/2-30,60,60,ST7735_WHITE);
+   tft.drawRect(w-60,h/2-30,60,60,ST7735_WHITE);
 }
 
 void loop() {
-// delay(100);
+   if(digitalRead(lb)) {
+      tft.fillRect(0,h/2-30,60,60,ST7735_RED);
+   }
+   else {
+      tft.fillRect(0,h/2-30,60,60,ST7735_WHITE);
+   }
+   if(digitalRead(cb)) {
+      tft.fillRect(w/2-30,h/2-30,60,60,ST7735_RED);
+   }
+   else {
+      tft.fillRect(w/2-30,h/2-30,60,60,ST7735_WHITE);
+   }
+   if(digitalRead(rb)) {
+      tft.fillRect(w-60,h/2-30,60,60,ST7735_RED);
+   }
+   else {
+      tft.fillRect(w-60,h/2-30,60,60,ST7735_WHITE);
+   }
 }
